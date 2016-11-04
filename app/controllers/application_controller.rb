75336639fdbb99eaa058c1da18156b4c988f3441
shyamahira <shyamahira@bbtower.co.jp>
2016-11-04 19:46:06 +0900

    エラー時のjsonデータのカスタマイズ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
     @current_resource_owner ||= User.find(resource_owner_id) if resource_owner_id
   end
 
+  def doorkeeper_unauthorized_render_options(error: nil)
+    status_code = case error.status
+                  when :unauthorized
+                    401
+                  when :forbidden
+                    403
+                  end
+
+    { json: { error: error.status, error_description: error.description }, status: status_code }
+  end
+
   rescue_from WeakParameters::ValidationError do
     head 400
   end
