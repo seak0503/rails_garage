60239dbb358102e8364e22e07bf0df6184b9edae
shyamahira <shyamahira@bbtower.co.jp>
2016-12-16 06:51:07 +0900

    ミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   use_doorkeeper
 
   scope :v1 do
-    resources :users, only: %i(index show create update destory) do
+    resources :users, only: %i(index show create update destroy) do
       resources :posts, only: %i(index)
     end
 
